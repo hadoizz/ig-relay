beeb130fad8ad23812e9b671bbbc9f918d12edb8
Artur <26014458+xout@users.noreply.github.com>
2020-02-01 23:52:05 +0100

    Custom scrollbar

--- www/client/components/Layout.tsx
+++ www/client/components/Layout.tsx
@@ -4,10 +4,22 @@ import Footer from './Layout/Footer'
 
 const useStyles = makeStyles((theme: Theme) => ({
   '@global': {
+    //for footer
     '#__next': {
       minHeight: '100vh',
       display: 'flex',
-      flexDirection: 'column'
+      flexDirection: 'column',
+    },
+    //scrollbar
+    '*::-webkit-scrollbar': {
+      width: '0.4em'
+    },
+    '*::-webkit-scrollbar-track': {
+      '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
+    },
+    '*::-webkit-scrollbar-thumb': {
+      backgroundColor: 'rgba(0,0,0,.1)',
+      outline: '1px solid slategrey'
     }
   },
   main: {
