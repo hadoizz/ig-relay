9835254a9ee0fce329e182d011d05277de693218
Artur <26014458+xout@users.noreply.github.com>
2020-02-01 23:41:04 +0100

    Improve appbar interface

--- www/client/components/Layout/AppBar.tsx
+++ www/client/components/Layout/AppBar.tsx
@@ -15,11 +15,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   grow: {
     flexGrow: 1
   },
-  appBar: {
-    //backgroundColor: theme.palette.background.paper,
-    //boxShadow: 'none',
-    //borderBottom: '1px solid #ddd'
-  },
   toolbar: {
     margin: 'auto',
     maxWidth: 960,
@@ -34,11 +29,13 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   logo: {
     display: 'inline',
-    //background: `radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)`,
     background: `radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 55%, #d6249f 90%)`,
     backgroundClip: 'text',
     '-webkit-background-clip': 'text',
-    '-webkit-text-fill-color': 'transparent'
+    '-webkit-text-fill-color': 'transparent',
+    [theme.breakpoints.down('xs')]: {
+      fontSize: '2.5rem'
+    }
   },
   drawer: {
     width: drawerWidth
@@ -53,7 +50,6 @@ const useStyles = makeStyles((theme: Theme) => ({
 }))
 
 export default () => {
-  const theme = useTheme()
   const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false)
 
   const toggleDrawer = () =>
@@ -61,94 +57,92 @@ export default () => {
 
   const classes = useStyles({})
   return (
-    <>
-      <AppBar className={classes.appBar} position="fixed" color="primary">
-        <Toolbar className={classes.toolbar}>
-          <Hidden implementation="css" mdUp>
-            <IconButton className={classes.burgerIcon} onClick={toggleDrawer}>
-              <Menu />
-            </IconButton>
-          </Hidden>
-          <Typography variant="h3" className={classes.logo}>
-            <Link href="/">
-              <a>
-                IFeed
+    <AppBar position="fixed" color="primary">
+      <Toolbar className={classes.toolbar}>
+        <Hidden implementation="css" mdUp>
+          <IconButton onClick={toggleDrawer}>
+            <Menu className={classes.burgerIcon} />
+          </IconButton>
+        </Hidden>
+        <Typography variant="h3" className={classes.logo}>
+          <Link href="/">
+            <a>
+              IFeed
+            </a>
+          </Link>
+        </Typography>
+        <div className={classes.grow} />
+        <Hidden smDown implementation="css">
+          <Link href="/dev">
+            <a>
+              <IconButton color="secondary">
+                <PhonelinkSetupOutlined />
+              </IconButton>
+            </a>
+          </Link>
+          <Link href="/profile">
+            <a>
+              <IconButton color="secondary">
+                <AccountCircleOutlined />
+              </IconButton>
+            </a>
+          </Link>
+          <IconButton color="secondary" onClick={logout}>
+            <ExitToAppOutlined />
+          </IconButton>
+        </Hidden>
+      </Toolbar>
+      { /* mobile drawer */ }
+      <Hidden smUp implementation="css">
+        <SwipeableDrawer open={mobileDrawerOpen} onOpen={toggleDrawer} onClose={toggleDrawer} className={classes.drawer}>
+          <List className={classes.drawer}>
+            <Link href="/bot">
+              <a className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <HomeOutlined color="secondary" />
+                  </ListItemIcon>
+                  <ListItemText>
+                    Bot
+                  </ListItemText>
+                </ListItem>
               </a>
             </Link>
-          </Typography>
-          <div className={classes.grow} />
-          <Hidden smDown implementation="css">
             <Link href="/dev">
-              <a>
-                <IconButton color="secondary">
-                  <PhonelinkSetupOutlined />
-                </IconButton>
+              <a className={classes.link}>
+                <ListItem button>
+                  <ListItemIcon>
+                    <PhonelinkSetupOutlined color="secondary" />
+                  </ListItemIcon>
+                  <ListItemText>
+                    Dev
+                  </ListItemText>
+                </ListItem>
               </a>
             </Link>
             <Link href="/profile">
-              <a>
-                <IconButton color="secondary">
-                  <AccountCircleOutlined />
-                </IconButton>
-              </a>
-            </Link>
-            <IconButton color="secondary" onClick={logout}>
-              <ExitToAppOutlined />
-            </IconButton>
-          </Hidden>
-        </Toolbar>
-        { /* mobile drawer */ }
-        <Hidden smUp implementation="css">
-          <SwipeableDrawer open={mobileDrawerOpen} onOpen={() => setMobileDrawerOpen(true)} onClose={() => setMobileDrawerOpen(false)} className={classes.drawer}>
-            <List className={classes.drawer}>
-              <Link href="/bot">
-                <a className={classes.link}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <HomeOutlined color="secondary" />
-                    </ListItemIcon>
-                    <ListItemText>
-                      Bot
-                    </ListItemText>
-                  </ListItem>
-                </a>
-              </Link>
-              <Link href="/dev">
-                <a className={classes.link}>
-                  <ListItem button>
-                    <ListItemIcon>
-                      <PhonelinkSetupOutlined color="secondary" />
-                    </ListItemIcon>
-                    <ListItemText>
-                      Dev
-                    </ListItemText>
-                  </ListItem>
-                </a>
-              </Link>
-              <Link href="/profile">
-                <a className={classes.link}>
-                <ListItem button>
-                  <ListItemIcon>
-                    <AccountCircleOutlined color="secondary" />
-                  </ListItemIcon>
-                  <ListItemText>
-                    My profile
-                  </ListItemText>
-                </ListItem>
-                </a>
-              </Link>
-              <ListItem button onClick={logout}>
+              <a className={classes.link}>
+              <ListItem button>
                 <ListItemIcon>
-                  <ExitToAppOutlined color="secondary" />
+                  <AccountCircleOutlined color="secondary" />
                 </ListItemIcon>
                 <ListItemText>
-                  Log out
+                  My profile
                 </ListItemText>
               </ListItem>
-            </List>
-          </SwipeableDrawer>
-        </Hidden>
-      </AppBar>
-    </>
+              </a>
+            </Link>
+            <ListItem button onClick={logout}>
+              <ListItemIcon>
+                <ExitToAppOutlined color="secondary" />
+              </ListItemIcon>
+              <ListItemText>
+                Log out
+              </ListItemText>
+            </ListItem>
+          </List>
+        </SwipeableDrawer>
+      </Hidden>
+    </AppBar>
   )
 }
