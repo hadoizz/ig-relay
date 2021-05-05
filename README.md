6ffdad9eb14fd477c29c07bf0c0afccb375deeeb
Artur <arturzietkiewicz@outlook.com>
2021-05-05 14:24:30 +0200

    Merge front-end with back-end on production

---
+++ README.md
@@ -0,0 +1,15 @@
+## Development
+
+```bash
+npm run dev
+```
+
+Two servers will be hosted: first that contains the back-end (./www) at localhost:8080 and the second one with the front-end (./www/client) at localhost:3000.
+
+## Production
+
+Insted of two servers, there will be only one server that merges the front-end with the back-end.
+
+Please set `REACT_APP_SERVER` environment variable to `/` and recompile everything with `npm run build`. This variable tells the client that it should send all requests over the same domain as it is hosted on. Also, don't forget to set `NODE_ENV` to `production`.
+
+To start the program run `npm start`.
--- www/src/app.module.ts
+++ www/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AllExceptionsFilter } from './all-exception.filter';
 import { AppService } from './app.service';
 import { BotsModule } from './bots/bots.module';
 import { StreamingModule } from './streaming/streaming.module';
 
 @Module({
-  imports: [BotsModule, StreamingModule],
+  imports: [
+    BotsModule,
+    StreamingModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'build'),
+    }),
+  ],
   providers: [
     AppService,
     {
